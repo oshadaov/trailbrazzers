30320f0818894194de1f001d62c56ed31a976ec9
oshada <oshada.viduranga@inovaitsys.com>
2026-01-25 11:11:46 +0530

    fifth one

--- src/data/tours.js
+++ src/data/tours.js
@@ -264,7 +264,7 @@ export const tourData = {
         duration: "Full Day",
         people: "Up to 8",
         highlights: ["Custom Animal Checklist", "Conservation Talk", "Transit Home"],
-        includes: ["Educational Worksheets", "Gifts for Kids", "Sustainable Farm Lunch1"],
+        includes: ["Educational Worksheets", "Gifts for Kids", "Sustainable Farm Lunch"],
         priceFrom: "Contact for rates",
       },
     ],
